92d2db78dfbbbc304b7b5ae15059b96f8baf5f53
Rishank Kumar <rishank.kumar@gmail.com>
2023-11-11 15:18:53 +0530

    added db config in seperate file

--- movie/movie.js
+++ movie/movie.js
@@ -4,14 +4,35 @@ const movieRoutes = require('./routes/movieRoutes.js');
 const app = express()
 const port = 5000
 
-const mongoDB = "mongodb://127.0.0.1/movies";
-mongoose.connect(mongoDB);
+const dbConfig = require('./db.config.js');
+
+mongoose.connect(dbConfig.url, {
+  useNewUrlParser: true
+}).then(() => {
+  console.log("Successfully connected to the database");    
+}).catch(err => {
+  console.log('Could not connect to the database.', err);
+  process.exit();
+});
 
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: false
 })); 
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use('/', movieRoutes);
 
